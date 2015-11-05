532786880e77a1a00f058c527408fb71fd84973b
Jarett Lee <googleplex_unknown@yahoo.com>
2015-11-04 20:10:38 -0500

    add games framework

--- src/games/Game.java
+++ src/games/Game.java
@@ -2,11 +2,11 @@
 package games;
 
 /**
- * Created by Jarett Lee on 11/3/2015
- * Last Edited by Jarett Lee 11/04/2015
- */
-
-
+* Game Object.
+*
+* @author
+* @version 0.0
+*/
 public class Game {
 
    public static void main(String[] args) {
---
+++ src/games/PlayerSide.java
@@ -0,0 +1,12 @@
+
+package games;
+
+/**
+* PlayerSide Object.
+*
+* @author
+* @version 0.0
+*/
+public class PlayerSide {
+
+}
