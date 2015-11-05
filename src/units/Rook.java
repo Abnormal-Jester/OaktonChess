4bff596784f1e35c8c9527d1f45e820e8108c31a
Jarett Lee <googleplex_unknown@yahoo.com>
2015-11-04 20:08:44 -0500

    change units javadoc info

--- src/units/Rook.java
+++ src/units/Rook.java
@@ -2,11 +2,11 @@
 package units;
 
 /**
- * Created by Jarett Lee on 11/4/2015
- * Last Edited by Jarett Lee 11/04/2015
- */
-
-
+* Rook Unit Object.
+*
+* @author
+* @version 0.0
+*/
 public class Rook extends Unit{
 
 }
--- src/units/Unit.java
+++ src/units/Unit.java
@@ -2,11 +2,11 @@
 package units;
 
 /**
- * Created by Jarett Lee on 11/4/2015
- * Last Edited by Jarett Lee 11/04/2015
- */
-
-
+* Unit Object.
+*
+* @author
+* @version 0.0
+*/
 public class Unit {
 
 }
