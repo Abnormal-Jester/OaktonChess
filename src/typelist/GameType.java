98d9c697847a03b84156ce734836e48e0a3e0939
Jarett Lee <googleplex_unknown@yahoo.com>
2015-11-04 19:40:13 -0500

    Change Edit Date

--- src/typelist/GameType.java
+++ src/typelist/GameType.java
@@ -4,6 +4,7 @@ package typelist;
 /**
  * Created by Jarett on 10/21/2015.
  */
+
 public enum GameType {
    NORMAL, GOD
 }
--- src/typelist/UnitType.java
+++ src/typelist/UnitType.java
@@ -3,9 +3,9 @@ package typelist;
 
 /**
  * Created by Jarett on 10/20/2015.
- * Absorbed in TypeList.java by Jarett on 10/21/2015
- * Created by Jarett on 10/21/2015
+ * Last Edited by Jarett on 10/21/2015
  */
+
 public enum UnitType {
    PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, GOD
 }
