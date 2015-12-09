ecbbf801b3cdfd3da1517b96e63756e71dad1aaf
ApolloZhu <zhuzhiyu20010417@outlook.com>
2015-12-09 16:05:12 -0500

    issue #2 finished

--- learn/BoardGUI.java
+++ learn/BoardGUI.java
@@ -12,18 +12,41 @@ import java.awt.Color;
 public class BoardGUI extends Frame{
    
    public BoardGUI () {
-      Frame frame = new Frame("This is the title");
-      frame.setBackground(Color.white);
-      frame.setLayout(null);
-      frame.setLocation(0,0);
-      frame.setSize(300,300);
-      TextField textfield = new TextField("String", 30);
-		textfield.setBackground(Color.gray);
-		textfield.setLocation(50, 125);
-		textfield.setSize(200, 50);
-		frame.add(textfield);
-		textfield.setVisible(true);
-      frame.setVisible(true);
+	Frame frame = new Frame("Frame Title");
+	frame.setBackground(Color.white);
+	frame.setLayout(null);
+	frame.setLocation(0, 0);
+	frame.setSize(300, 300);
+	TextField textfield = new TextField("0", 30);
+	textfield.setBackground(Color.gray);
+	textfield.setForeground(Color.white);
+	textfield.setLocation(50, 125);
+	textfield.setSize(200, 50);
+	textfield.setEditable(false);
+	Button countButton = new Button("tap me");
+	countButton.setLocation(100, 190);
+	countButton.setSize(100, 44);
+	countButton.setForeground(Color.blue);
+	countButton.addActionListener(new ActionListener(){ 
+		public void actionPerformed(ActionEvent e) {
+			int current = Integer.parseInt(textfield.getText());
+			textfield.setText(String.valueOf(current + 1));
+		}
+	});
+	Button exitButton = new Button("Exit");
+	exitButton.setLocation(100, 245);
+	exitButton.setSize(100, 44);
+	exitButton.setForeground(Color.blue);
+	exitButton.addActionListener(new ActionListener(){ 
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
+	});
+	frame.add(exitButton);
+	frame.add(textfield);
+	frame.add(countButton);
+	textfield.setVisible(true);
+	frame.setVisible(true);
    }
 
    public static void main(String[] args) {
